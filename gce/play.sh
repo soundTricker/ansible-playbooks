c9c43766af124d197c90cf25c7fa0802945f9db8
keisuke oohashi <keisuke.oohashi@gmail.com>
2014-08-22 10:07:09 +0900

    gce dir init

---
+++ gce/play.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PLAYBOOK="$1"
+
+if [ -z $PLAYBOOK ]; then
+  echo "You need to pass a playback as argument to this script."
+  exit 1
+fi
+
+export PYTHONPATH=$(pwd)/credentials:$PYTHONPATH
+export GCE_INI_PATH=$(pwd)/inventory/gce.ini
+export SSL_CERT_FILE=$(pwd)/credentials/cacert.pem
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+if [ ! -f "$SSL_CERT_FILE" ]; then
+  curl -O http://curl.haxx.se/ca/cacert.pem
+fi
+
+sudo ansible-playbook -vvvv -i inventory/ "$PLAYBOOK"
+
